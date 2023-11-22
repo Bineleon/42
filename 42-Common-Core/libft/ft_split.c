f1c273ab36dac1748cd959d8ff64fb0cbdfdf50e
Nelbi Leon <nelbi.leon@gmail.com>
2023-11-22 17:05:13 +0100

    ONGOING ft_split

--- 42-Common-Core/libft/ft_split.c
+++ 42-Common-Core/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:43:29 by neleon            #+#    #+#             */
-/*   Updated: 2023/11/20 17:15:48 by neleon           ###   ########.fr       */
+/*   Updated: 2023/11/21 17:57:10 by neleon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,6 @@
 // character ’c’ as a delimiter. The array must end
 // with a NULL pointer.
 
-size_t		ft_strlen(const char *str);
-
-static int	ft_count_words(char const *s, char c)
-{
-	int	count;
-	int	i;
-
-	count = 1;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			count++;
-		i++;
-	}
-	return (count);
-}
-
 static int	ft_word_len(char const *s, char c)
 {
 	int	len;
@@ -53,28 +35,109 @@ static int	ft_word_len(char const *s, char c)
 	return (len);
 }
 
-static  ft_word(char const *s, char c)
+static  char *ft_word(char const *s, char c)
 {
     char *word;
     int i;
 
     i = 0;
     word = (char *)malloc((ft_word_len(s, c) + 1) * sizeof(char));
+    if (!word)
+        return (NULL);
+    while (s[i] && s[i] != c)
+    {
+        word[i] = s[i];
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
+}
+static int	ft_count_words(char const *s, char c)
+{
+	int	count;
+	int	i;
 
-    //  TO DO
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+        if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
+            count++;
+		i++;
+	}
+	return (count);
+}
+// static void	freesplit(char **split, int words)
+// {
+// 	int	i;
 
+// 	i = -1;
+// 	while (++i <= words)
+// 	{
+// 		free(split[i]);
+// 		split[i] = NULL;
+// 	}
+// 	free(split);
+// 	split = NULL;
+// }
+
+static void free_malloc(char **split, int size)
+{
+    int i;
+
+    i = 0;
+    if (split != NULL)
+    {
+        while (i < size)
+        {
+            free(split[i]);
+            i++;
+        }
+        free(split);
+    }
 }
 
-// char **ft_split(char const *s, char c)
-// {
+char **ft_split(char const *s, char c)
+{
+    char **words;
+    int word_count;
+    int i;
+    int j;
 
-// }
+    i = 0;
+    j = 0;
+    word_count = ft_count_words(s, c);
+    words = (char **)malloc((word_count + 1) * sizeof(char *));
+    if (!words)
+        return (NULL);
+
+    while (j < word_count)
+    {
+        while (s[i] == c)
+            i++;
+        words[j] = ft_word(&s[i], c);
+        i += ft_word_len(&s[i], c);
+        j++;
+    }
+    words[word_count] = NULL;
+    return (words);
+}
 
 int	main(void)
 {
 	char	c;
+    int i;
 
 	c = ' ';
 	char const s[] = "Coucou me voila";
-	printf("Word lenght: %d\n", ft_word_len(s, c));
+    char **split = ft_split(s, c);
+    i = 0;
+    printf("To split: %s\n", s);
+    printf("Word count: %d\nSplit: \n\n", ft_count_words(s, c));
+    while (i < ft_count_words(s, c))
+    {
+	    printf("        %s\n", split[i]);
+        i++;
+    }
+    return (0);
 }
