dba944027390e38120301d84149c053db6dfd363
Nelbi Leon <nelbi.leon@gmail.com>
2024-01-05 13:16:26 +0100

    gnl first commit

---
+++ 42-Common-Core/get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 18:59:03 by neleon            #+#    #+#             */
+/*   Updated: 2024/01/05 13:15:23 by neleon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
