f643a47ea81682e553aa09b8774b64de957e38f2
Nelbi Leon <nelbi.leon@gmail.com>
2024-01-12 13:49:18 +0100

    gnl function 1st batch

--- 42-Common-Core/get_next_line/get_next_line.h
+++ 42-Common-Core/get_next_line/get_next_line.h
@@ -6,17 +6,32 @@
 /*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 18:59:03 by neleon            #+#    #+#             */
-/*   Updated: 2024/01/05 13:15:23 by neleon           ###   ########.fr       */
+/*   Updated: 2024/01/10 19:21:02 by neleon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 
-char	*get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+	{
+		/* data */
+	};
+
+}					t_list;
+
+char				*get_next_line(int fd);
+void				ft_bzero(void *s, size_t n);
 
 #endif
--- 42-Common-Core/get_next_line/get_next_line_utils.c
+++ 42-Common-Core/get_next_line/get_next_line_utils.c
@@ -6,7 +6,27 @@
 /*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 13:15:43 by neleon            #+#    #+#             */
-/*   Updated: 2024/01/05 13:15:44 by neleon           ###   ########.fr       */
+/*   Updated: 2024/01/10 17:35:58 by neleon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char *calloc_gnl()
+{
+    
+}
+
+void ft_bzero(void *s, size_t n)
+{
+    size_t i;
+    unsigned char *ptr;
+
+    i = 0;
+    ptr = (unsigned char *) s;
+    while (i < n)
+    {
+        ptr[i] = 0;
+        i++;
+    }
+}
