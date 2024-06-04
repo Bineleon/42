142a7c5e8d8fe8a11033dab642bd46dbb65e69e7
Nelbi Leon <nelbi.leon@gmail.com>
2024-06-04 16:26:20 +0200

    Minitalk nakefile

---
+++ 42-Common-Core/04_Minitalk/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 11:37:15 by neleon            #+#    #+#             */
+/*   Updated: 2024/06/03 11:39:21 by neleon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MINITALK_H
+#define MINITALK_H
+
+# include "libft/ft_printf/srcs/ft_printf.h"
+# include "libft/gnl/get_next_line_bonus.h"
+# include "libft/libft/libft.h"
+
+
+#endif
